85789b660df1c686a3989fb294253be3250fbe71
Djal4 <aleksandar.djalovic@quantoxtechnology.com>
2021-09-16 13:10:28 +0200

    Variable names have been changed.

--- Index.php
+++ Index.php
@@ -1,40 +1,40 @@
 <?php
 require_once("includer.php");
-$krug=new \bodies\Circle(3);
+$circle=new \bodies\Circle(3);
 echo "Circle<br>";
 echo "Side length: 3cm";
 echo "<br>";
-echo "Volume: ".$krug->volume()."cm";
+echo "Volume: ".$circle->volume()."cm";
 echo "\n";
-echo "Surface: ".$krug->surface()."cm";
+echo "Surface: ".$circle->surface()."cm";
 echo "<br>";
 echo "<br>";
 
-$tr=new \bodies\Triangle(3,4,5);
+$triangle=new \bodies\Triangle(3,4,5);
 echo "Triangle<br>";
 echo "Sides lengths: 3cm,4cm,5cm";
 echo "<br>";
-echo "Volume: ".$tr->volume()."cm";
+echo "Volume: ".$triangle->volume()."cm";
 echo "\n";
-echo "Surface: ".$tr->surface()."cm";
+echo "Surface: ".$triangle->surface()."cm";
 echo "<br>";
 echo "<br>";
 
-$prav=new \bodies\Rectangle(4,3);
+$rectangle=new \bodies\Rectangle(4,3);
 echo "Rectangle<br>";
 echo "Sides lengths: 3cm,4cm";
 echo "<br>";
-echo "Volume: ".$prav->volume()."cm";
+echo "Volume: ".$rectangle->volume()."cm";
 echo "\n";
-echo "Surface: ".$prav->surface()."cm";
+echo "Surface: ".$rectangle->surface()."cm";
 echo "<br>";
 echo "<br>";
 
-$sa= new \bodies\Polygon(8,6,6,6,6,6,6,6);
+$polygon= new \bodies\Polygon(8,6,6,6,6,6,6,6);
 echo "Polygon<br>";
 echo "Sides lengths: 6cm(6 sides), height:8cm";
 echo "<br>";
-echo "Volume: ".$sa->volume()."cm";
+echo "Volume: ".$polygon->volume()."cm";
 echo "\n";
-echo "Surface: ".$sa->surface()."cm";
+echo "Surface: ".$polygon->surface()."cm";
 ?>
